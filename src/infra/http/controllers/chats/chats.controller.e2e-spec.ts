f5c3b69e2964f074daa0ab1e787aaaf973c69fe6
Rodrigo Oliveira <rodrigofeitosa.contato@gmail.com>
2024-08-27 18:40:51 -0300

    test: create tests to routes to get room, create room, join room, delete room and search rooms with same name

--- src/infra/http/controllers/chats/chats.controller.e2e-spec.ts
+++ src/infra/http/controllers/chats/chats.controller.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { INestApplication } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
+import { User } from '@prisma/client'
 import request from 'supertest'
 
 import { AppModule } from '@/infra/app.module'
@@ -10,6 +11,8 @@ describe('Chats Controller (E2E)', () => {
   let app: INestApplication
   let prisma: PrismaService
   let jwt: JwtService
+  let user: User
+  let accessToken: string
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -21,25 +24,29 @@ describe('Chats Controller (E2E)', () => {
     jwt = moduleRef.get(JwtService)
 
     await app.init()
-  })
 
-  test('[POST] /chats', async () => {
-    const user = await prisma.user.create({
+    user = await prisma.user.create({
       data: {
         name: 'John Doe',
-        email: 'johndoe2@example.com',
+        email: 'johndoe@example.com',
         password: '123456',
       },
     })
 
-    const accessToken = jwt.sign({ sub: user.id })
+    accessToken = jwt.sign({ sub: user.id })
+  })
 
+  let createdRoomId: string
+
+  test('[POST] /chats', async () => {
     const response = await request(app.getHttpServer())
       .post('/chats')
       .set('Authorization', `Bearer ${accessToken}`)
       .send({
         name: 'New Test Room',
       })
+
+    createdRoomId = response.body.room.id
 
     expect(response.statusCode).toBe(201)
     expect(response.body).toMatchObject({
@@ -48,5 +55,64 @@ describe('Chats Controller (E2E)', () => {
         ownerId: user.id,
       }),
     })
+  })
+
+  test('[POST] /chats/:roomId/join', async () => {
+    const user2 = await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'johndoe2@example.com',
+        password: '123456',
+      },
+    })
+
+    const accessToken = jwt.sign({ sub: user2.id })
+
+    const response = await request(app.getHttpServer())
+      .post(`/chats/${createdRoomId}/join`)
+      .set('Authorization', `Bearer ${accessToken}`)
+
+    expect(response.statusCode).toBe(201)
+  })
+
+  test('[GET] /chats/:roomId', async () => {
+    const response = await request(app.getHttpServer())
+      .get(`/chats/${createdRoomId}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toMatchObject({
+      room: expect.objectContaining({
+        name: 'New Test Room',
+        id: createdRoomId,
+      }),
+    })
+  })
+
+  test('[GET] /chats?name=room%to%search', async () => {
+    const response = await request(app.getHttpServer())
+      .get(`/chats`)
+      .query({ name: 'New Test Room' })
+      .set('Authorization', `Bearer ${accessToken}`)
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toMatchObject({
+      rooms: [expect.objectContaining({ name: 'New Test Room' })],
+    })
+  })
+
+  test('[DELETE] /chats/:roomId', async () => {
+    const response = await request(app.getHttpServer())
+      .delete(`/chats/${createdRoomId}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+
+    const roomOnDatabase = await prisma.room.findUnique({
+      where: {
+        id: createdRoomId,
+      },
+    })
+
+    expect(response.statusCode).toBe(200)
+    expect(roomOnDatabase).toBe(null)
   })
 })
