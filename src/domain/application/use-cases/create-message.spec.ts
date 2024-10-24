8027f19487a94fbf4f0e22f34803f49eafcf9dc8
Rodrigo Oliveira <rodrigofeitosa.contato@gmail.com>
2024-10-24 13:40:30 -0300

    test: update test to create a user when testing message creation

--- src/domain/application/use-cases/create-message.spec.ts
+++ src/domain/application/use-cases/create-message.spec.ts
@@ -1,31 +1,39 @@
 import { makeRoom } from 'test/factories/makeRooms'
+import { makeUser } from 'test/factories/makeUser'
 import { InMemoryMessagesRepository } from 'test/repositories/in-memory-messages-repository'
 import { InMemoryRoomsRepository } from 'test/repositories/in-memory-rooms-repository'
+import { InMemoryUsersRepository } from 'test/repositories/in-memory-users-repository'
 
 import { CreateMessageUseCase } from './create-message'
 
 let inMemoryMessagesRepository: InMemoryMessagesRepository
 let inMemoryRoomsRepository: InMemoryRoomsRepository
+let inMemoryUsersRepository: InMemoryUsersRepository
 let sut: CreateMessageUseCase
 
 beforeEach(() => {
   inMemoryMessagesRepository = new InMemoryMessagesRepository()
   inMemoryRoomsRepository = new InMemoryRoomsRepository()
+  inMemoryUsersRepository = new InMemoryUsersRepository()
+
   sut = new CreateMessageUseCase(
     inMemoryMessagesRepository,
+    inMemoryUsersRepository,
     inMemoryRoomsRepository,
   )
 })
 
 describe('Create Message', () => {
   it('should be able to create a message', async () => {
+    const user = makeUser()
     const room = makeRoom()
 
+    await inMemoryUsersRepository.create(user)
     await inMemoryRoomsRepository.create(room)
 
     const { message } = await sut.execute({
       roomId: room.id,
-      authorId: 'custom-author-1',
+      authorId: user.id,
       content: 'new message',
     })
 
